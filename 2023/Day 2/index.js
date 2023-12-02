59798a73b5d2814a38353428e875ee76eff2c798
Alan Fitzpatrick <L00162800@student.lyit.ie>
2023-12-02 21:50:54 +0000

    Updated with part 1 completed

--- 2023/Day 2/index.js
+++ 2023/Day 2/index.js
@@ -1,10 +1,18 @@
 const fs = require('fs')
-const input = fs.readFileSync('./input.txt', 'utf-8').split('\n')
+const inputRows = fs.readFileSync('./input.txt', 'utf-8').split('\n')
 
-function part1() {
+function part1(input) {
+    const loadOut = { red: 12, green: 13, blue: 14 }
+    return input
+        .filter(row => row.split(':')[1].split(/,|;/)
+            .every(load => (([num, word]) => parseInt(num) <= loadOut[word])(load.trim().split(' ')))
+        ).reduce((acc, curr) => acc + parseInt(curr.split(':')[0].split(' ')[1]), 0)
+}
+
+function part2(input) {
 
 }
 
-function part2() {
+console.log(part1(inputRows)) // 2512
 
-}
+console.log(part2(inputRows))
